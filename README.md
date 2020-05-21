71ebb46ecbae81f27edba0ca2781d25bb20db5f4
yc.huang71 <yc.huang71@gmail.com>
2020-05-21 18:18:57 +0800

    add teleop_twist_keyboard and twist_mux

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 ```bash
 sudo apt install jstest-gtk
 sudo apt install ros-kinetic-joy
+sudo apt install ros-kinetic-teleop-twist-keyboard
 sudo apt install ros-kinetic-twist-mux
 ```
--- scripts/teleop_joy.py
+++ scripts/teleop_joy.py
@@ -9,18 +9,18 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
 
 def callback(data):
-    if abs(data.axes[5]) > 0.1:  # increase VEL_X 10%
-        global VEL_X
-        VEL_X = abs(VEL_X*(data.axes[5] + VEL_X_INCR))
-        rospy.loginfo("current max linear speed: %f", VEL_X)
-    elif abs(data.axes[4]) > 0.1:  # increase ROT_Z 10%
-        global ROT_Z
-        ROT_Z = abs(ROT_Z*(data.axes[4] + ROT_Z_INCR))
-        rospy.loginfo("current max angular speed: %f", ROT_Z)
+    if abs(data.axes[5]) > 0.1:
+        global SPEED
+        SPEED = abs(SPEED*(data.axes[5] + 0.1))  # increase speed by 10%
+        rospy.loginfo("current max linear speed: %f", SPEED)
+    elif abs(data.axes[4]) > 0.1:
+        global TURN
+        TURN = abs(TURN*(data.axes[4] + 0.1))  # increase turn by 10%
+        rospy.loginfo("current max angular speed: %f", TURN)
     else:
         twist = Twist()
-        twist.linear.x = data.axes[1] * VEL_X
-        twist.angular.z = data.axes[0] * ROT_Z
+        twist.linear.x = data.axes[1] * SPEED
+        twist.angular.z = data.axes[0] * TURN
         rospy.loginfo("twist.linear: %f ; angular %f", twist.linear.x, twist.angular.z)
         PUB.publish(twist)
 
@@ -28,13 +28,10 @@ if __name__ == '__main__':
 
     rospy.init_node(name="teleop_joy", anonymous=True)
 
-    topic = rospy.get_param(param_name="~topic")
-    VEL_X = rospy.get_param(param_name="~vel_x")
-    ROT_Z = rospy.get_param(param_name="~rot_z")
-    VEL_X_INCR = rospy.get_param(param_name="~vel_x_incr")
-    ROT_Z_INCR = rospy.get_param(param_name="~rot_z_incr")
+    SPEED = rospy.get_param(param_name="~speed")
+    TURN = rospy.get_param(param_name="~turn")
 
-    PUB = rospy.Publisher(name=topic, data_class=Twist, queue_size=1)
+    PUB = rospy.Publisher(name="cmd_vel", data_class=Twist, queue_size=1)
     rospy.Subscriber(name="joy", data_class=Joy, callback=callback, queue_size=1)
 
     rospy.spin()
