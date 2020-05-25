45dc42ba661e79a9943f613c00b6febd0992aa2c
yc.huang71 <yc.huang71@gmail.com>
2020-05-25 13:08:38 +0800

    teleop_joy.py skip continuous zero command

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # ros_teleop
 
+## Requirements
+
 ```bash
 sudo apt install jstest-gtk
 sudo apt install ros-kinetic-joy
 sudo apt install ros-kinetic-teleop-twist-keyboard
 sudo apt install ros-kinetic-twist-mux
 ```
+
+## Note
+
+The teleop_twist_keyboard will continuously publish cmd_vel when hold the key pressed. The joy_node can use parameter autorepeat_rate to resend the non-changing state. This two informations are important for twist_mux.
--- scripts/teleop_joy.py
+++ scripts/teleop_joy.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 """
-Node converts joystick inputs into commands for Turtlesim
+Node converts joy inputs.
+Caution:
+    The parameter deadzone of joy should not equal to zero.
+    Make sure the release state of joy can publish the zero command.
 """
 
 import rospy
@@ -10,26 +13,37 @@ from sensor_msgs.msg import Joy
 
 def callback(data):
     if abs(data.axes[5]) > 0.1:
-        global SPEED
-        SPEED = abs(SPEED*(data.axes[5] + 0.1))  # increase speed by 10%
-        rospy.loginfo("current max linear speed: %f", SPEED)
+        global VX_MAX
+        VX_MAX = abs(VX_MAX*(data.axes[5] + 0.1))  # increase speed by 10%
+        rospy.loginfo("current max linear speed: %f", VX_MAX)
     elif abs(data.axes[4]) > 0.1:
-        global TURN
-        TURN = abs(TURN*(data.axes[4] + 0.1))  # increase turn by 10%
-        rospy.loginfo("current max angular speed: %f", TURN)
+        global WZ_MAX
+        WZ_MAX = abs(WZ_MAX*(data.axes[4] + 0.1))  # increase turn by 10%
+        rospy.loginfo("current max angular speed: %f", WZ_MAX)
     else:
+        global VX_LAST
+        global WZ_LAST
         twist = Twist()
-        twist.linear.x = data.axes[1] * SPEED
-        twist.angular.z = data.axes[0] * TURN
-        rospy.loginfo("twist.linear: %f ; angular %f", twist.linear.x, twist.angular.z)
-        PUB.publish(twist)
+        vx = data.axes[1] * VX_MAX
+        wz = data.axes[0] * WZ_MAX
+        if vx == 0.0 and wz == 0.0 and VX_LAST == 0.0 and WZ_LAST == 0.0:
+            pass
+        else:
+            twist.linear.x = vx
+            twist.angular.z = wz
+            rospy.loginfo("twist.linear: %f ; angular %f", twist.linear.x, twist.angular.z)
+            PUB.publish(twist)
+        VX_LAST = vx
+        WZ_LAST = wz
 
 if __name__ == '__main__':
 
     rospy.init_node(name="teleop_joy", anonymous=True)
 
-    SPEED = rospy.get_param(param_name="~speed")
-    TURN = rospy.get_param(param_name="~turn")
+    VX_MAX = rospy.get_param(param_name="~vx_max")
+    WZ_MAX = rospy.get_param(param_name="~wz_max")
+    VX_LAST = 0.0
+    WZ_LAST = 0.0
 
     PUB = rospy.Publisher(name="cmd_vel", data_class=Twist, queue_size=1)
     rospy.Subscriber(name="joy", data_class=Joy, callback=callback, queue_size=1)
